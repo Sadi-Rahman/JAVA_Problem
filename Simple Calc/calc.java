122cbcf4e5fccf11ca0e6c5df5d2b90434ad247f
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2022-05-27 15:03:14 +0000

    added simple calc

---
+++ Simple Calc/calc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class main{ 
+    public static void main(String[] args) {
+    char type;
+    double num1, num2, res;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your type: + - * / ");
+
+
+    }
+}
